290cdc2cca40f16ec4251f192ae4bd2d2ddd60b5
InnaShp <107990414+InnaShp@users.noreply.github.com>
2023-11-17 18:13:02 +0200

    Add header and items for Calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Calendar2Date, CaretUp } from 'react-bootstrap-icons';
+import { calendarItems } from '../constants';
 
 const Calendar = () => {
+  
   return (
     <div className='Calendar'>
-      Calendar
+      <div className='header'>
+        <div className='title'>
+          <Calendar2Date size='18' />
+          <p>Calendar</p>
+        </div>
+        <div className='btns'>
+          <span className='title'>
+            <CaretUp size='20' />
+          </span>
+        </div>
+      </div>  
+      <div className='items'>
+        {
+          calendarItems.map(item =>
+            <div className='item' key={item}>
+              {item}
+            </div>
+          )
+        }
+      </div>
     </div>
   )
 }
